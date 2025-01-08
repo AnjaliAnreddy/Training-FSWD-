164d7f7f9eb7eecdcfe9751fe6ae706dfcb9a914
AnjaliAnreddy <anreddyanjali200427@gmail.com>
2025-01-08 12:04:42 +0530

    javascript content

---
+++ loopingstatement.js
@@ -0,0 +1,14 @@
+// for( let i=1;i<=5;i++){
+//     console.log("Hello World!");
+// }
+
+//!WAJP  to find a factorial of a number
+//! 5=120;5*4*3*2*1
+
+
+let n=5;
+let factorial=1;
+for(let i=1;i<=n;i++){
+    factorial*=i;
+}
+console.log("Factorial of number is " +factorial);
