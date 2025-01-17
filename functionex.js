05d08077fc5cf2e6ef86eda2689332e809fad0bb
AnjaliAnreddy <anreddyanjali200427@gmail.com>
2025-01-17 10:32:39 +0530

    javascrpit content

--- functionex.js
+++ functionex.js
@@ -110,8 +110,55 @@
 // let x = add(5,5);
 // console.log(x);
 
-let x = (a, b) => a + b; //! implicit return
-console.log(x(10, 30));
+// let x = (a, b) => a + b; //! implicit return
+// console.log(x(10, 30));
 
-let y = (a, b) => { return a + b; }//! explicit return
-console.log(y(3, 3));
+// let y = (a, b) => { return a + b; }//! explicit return
+// console.log(y(3, 3));
+
+// ! higher order function
+
+// function hof(a){
+    
+//     return a();
+// };
+
+//  let x = hof(function(){return "This is callback function"});
+//  console.log(x);
+
+// let b =  function(){
+//     console.log("Anonymus");
+//  };
+// b();
+
+// function add(callback){
+//     return callback(5,3);
+// };
+// let x = add(function(a,b){return a+b});
+// console.log(x);
+
+// let users = ["Navya" , "Vishnu" , "Keshav" , "Shravan(insta)" , "Shiva"];
+// let x = users.map((user)=>{
+//     // console.log(user);
+//     return user;
+// });
+// console.log(x);
+// let x = users.forEach((user)=>{
+//     return (user);
+// });
+// console.log(x);
+
+
+var a =10;
+let b= 30;
+function x(){
+    var user = "Shravan";
+    let company = "Amazon";
+    const sal = 1234567890;
+    console.log(user);
+    console.log(company);
+    console.log(sal);
+
+    console.log(a , b);
+}
+x();
