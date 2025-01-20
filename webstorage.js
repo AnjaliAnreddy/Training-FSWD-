4b90b05bad0072ac10e6045cc38350c56f51af3c
AnjaliAnreddy <anreddyanjali200427@gmail.com>
2025-01-20 19:37:54 +0530

    javascrpit content

---
+++ webstorage.js
@@ -0,0 +1,28 @@
+// localStorage.setItem("user1","Raghu");
+// localStorage.setItem("user2","Vamshi");
+// localStorage.setItem("user3","Pavithra");
+// localStorage.setItem("user4","Sanjay");
+// localStorage.setItem("user5","Rishi");
+// localStorage.setItem("user6","Shravan");
+
+// let user6=localStorage.getItem("user6");
+// console.log(user6);
+
+// localStorage.removeItem("user1");
+
+//localStorage.clear();
+
+sessionStorage.setItem("user1","Raghu");
+sessionStorage.setItem("user2","Vamshi");
+sessionStorage.setItem("user3","Pavithra");
+sessionStorage.setItem("user4","Sanjay");
+sessionStorage.setItem("user5","Rishi");
+sessionStorage.setItem("user6","Anjali");
+
+let user6=localStorage.getItem("user6");
+console.log(user6);
+
+sessionStorage.removeItem("user1");
+
+//localStorage.clear();
+
